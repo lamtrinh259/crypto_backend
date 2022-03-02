d91bbb83f992bb546b102a0b5a8ee92ab0ceddf5
Lam Trinh <lam.trinh@global.t-bird.edu>
2022-03-02 12:34:10 +0900

    Get data function is implemented correctly the 3 currencies

--- crypto_backend/data.py
+++ crypto_backend/data.py
@@ -1,6 +1,6 @@
-# Pandas error
 import pandas as pd
 
+# Either URI or URL works
 BTC_USD_S3URI = "s3://cryptocurrency-forecasting/raw_data/btcusd.csv"
 BTC_USD_URL = "https://cryptocurrency-forecasting.s3.ap-northeast-1.amazonaws.com/raw_data/btcusd.csv"
 ETH_USD_S3URI = "s3://cryptocurrency-forecasting/raw_data/etcusd.csv"
@@ -8,20 +8,29 @@ ETH_USD_URL = "https://cryptocurrency-forecasting.s3.ap-northeast-1.amazonaws.co
 LTC_USD_S3URI = "s3://cryptocurrency-forecasting/raw_data/ltcusd.csv"
 LTC_USD_URL = "https://cryptocurrency-forecasting.s3.ap-northeast-1.amazonaws.com/raw_data/ltcusd.csv"
 
-def get_data(crypto, last_rows=5000):
-    """ Get dataset of selected crypto from cloud storage"""
-    # if crypto == "BTC":
-    # df = pd.read_csv(f'{crypto}_USD_path', nrows)
-    fname = f'{crypto}_USD_URL'
-    n_rows = sum(1 for row in open(fname, 'r'))
-    df = pd.read_csv(fname, skiprows=range(1, n_rows - last_rows))
+def get_data(crypto, last_rows=500000):
+    """ Get dataset of selected crypto from cloud storage
+    500,000 data points (mins) is roughly equivalent to 1 year worth of data
+    # Params: chosen crypto by the user from the front end """
+    if crypto == "BTC":
+        df_full = pd.read_csv(BTC_USD_URL)
+        n_rows = len(df_full)
+        df = pd.read_csv(BTC_USD_URL, skiprows=range(1, n_rows - last_rows))
+    elif crypto == 'ETH':
+        df_full = pd.read_csv(ETH_USD_URL)
+        n_rows = len(df_full)
+        df = pd.read_csv(ETH_USD_URL, skiprows=range(1, n_rows - last_rows))
+    elif crypto == 'LTC':
+        df_full = pd.read_csv(LTC_USD_S3URI)
+        n_rows = len(df_full)
+        df = pd.read_csv(LTC_USD_S3URI, skiprows=range(1, n_rows - last_rows))
     print(df.head())
     return df
 
 
 
 if __name__ == '__main__':
-    # Test function here
     # For testing
-    get_data('BTC')
-    pass
+    # get_data('BTC')
+    # get_data('ETH')
+    get_data('LTC')
