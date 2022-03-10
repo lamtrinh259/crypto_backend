bf12a10b5bb737f3e2f5ae4102945613a7958af9
Tony Ni <ni.tony.ym@gmail.com>
2022-03-10 15:53:23 +0900

    LSTM rows now ordered

--- crypto_backend/utils.py
+++ crypto_backend/utils.py
@@ -121,13 +121,10 @@ def reduce_mem_usage(df):
 
     return df
 
-
-# This train test generator's default assumes the following,
-# approximately 2 years of data is input into the function
-# It then makes 20 4-month-long train_test_split sets where the first 3 month is train and last month is test
-# each set is 1 month further than the previous set . this can be changed by changing the step
-
-def train_test_generator(X,test_sets=20,train_size=3*30*24*60,test_size=30*24*60,step = 30*24*60):
+def train_test_generator(X,test_sets=20,train_size=235,test_size=15,step = 3):
+    # This train test generator takes approx 2 years of data input into the function
+    # It then makes 20 5-month-long train_test_split sets where the first 3 month is train and last month is test
+    # each set is 1 month further than the previous set . this can be changed by changing the step
     X_train= []
     X_test=[]
     for i in range(test_sets):
@@ -137,8 +134,8 @@ def train_test_generator(X,test_sets=20,train_size=3*30*24*60,test_size=30*24*60
         X_train.append(X.iloc[(start):(start+train_size)])
         #testing set
         X_test.append(X.iloc[(start+train_size):(start+train_size+test_size)])
-
     return X_train,X_test
+
 
 #take in prediction take from facebook then graphs it together with previous data
 def FB_grapher(fb_data,currency):
